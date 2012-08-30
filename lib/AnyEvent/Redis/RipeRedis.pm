6d61c5272231b14c44f878bdc1278286c3d90892
Eugene Ponizovsky <eugene.ponizovsky@sms-online.com>
2012-08-31 00:25:07 +0400

    Fix in POD

--- lib/AnyEvent/Redis/RipeRedis.pm
+++ lib/AnyEvent/Redis/RipeRedis.pm
@@ -27,7 +27,7 @@ use fields qw(
   subs
 );
 
-our $VERSION = '1.101';
+our $VERSION = '1.102';
 
 use AnyEvent::Handle;
 use Encode qw( find_encoding is_utf8 );
@@ -1104,7 +1104,7 @@ Redis is loading the dataset in memory.
 
 =item E_IO_OPERATION
 
-I/O operation error. Connection closed.
+I/O operation error.
 
 =item E_CONN_CLOSED_BY_REMOTE_HOST
 
